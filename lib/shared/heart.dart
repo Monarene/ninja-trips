e35a4c83639f3e9ee6e41d466f8ec8b8ce4dbf89
Michael Mekuleyi <mekuleyimichael@gmail.com>
2021-08-29 07:47:40 +0100

    base animation works

--- lib/shared/heart.dart
+++ lib/shared/heart.dart
@@ -5,16 +5,69 @@ class Heart extends StatefulWidget {
   _HeartState createState() => _HeartState();
 }
 
-class _HeartState extends State<Heart> {
+class _HeartState extends State<Heart> with SingleTickerProviderStateMixin{
+  bool isFav = false;
+  AnimationController _controller;
+  Animation<Color> _colorAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    initializeVariables();
+    activateStatusListeners();
+  }
+
+  void initializeVariables() {
+    _controller = AnimationController(duration: Duration(milliseconds: 500), vsync: this );
+    _colorAnimation = ColorTween(begin: Colors.grey[400], end: Colors.red).animate(_controller);
+  }
+
+  void activateStatusListeners() {
+    _controller.addListener(() {
+    });
+
+    _controller.addStatusListener((status) {
+      if(status == AnimationStatus.completed){
+        changeFavStatetoTrue();
+      } if(status == AnimationStatus.dismissed) {
+        changeFavStatusToFalse();
+      }
+    });
+  }
+
+  void changeFavStatetoTrue() {
+    setState(() {
+      isFav = true;
+    });
+  }
+
+  void changeFavStatusToFalse() {
+    setState(() {
+      isFav = false;
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      icon: Icon(
-        Icons.favorite,
-        color: Colors.grey[400],
-        size: 30,
-      ),
-      onPressed: () {},
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (BuildContext context, _) {
+        return IconButton(
+          icon: Icon(
+            Icons.favorite,
+            color: _colorAnimation.value,
+            size: 30,
+          ),
+          onPressed: () {
+            isFav ? _controller.reverse() : _controller.forward();
+          },
+        );
+      },
     );
   }
 }
